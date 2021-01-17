966ca309978bcfb72c24f6e6a8ca49f2cb088418
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-16 22:51:45 -0700

    Updated category.sql
    Added the primary key, as well as sample queries for adding values.

--- speedraterAPI/src/dbqueries/category.sql
+++ speedraterAPI/src/dbqueries/category.sql
@@ -1,6 +1,26 @@
 -- Table Creation
 
 CREATE TABLE category (
-    categoryId SERIAL,
+    categoryId SERIAL PRIMARY KEY,
     categoryName TEXT
 )
+
+-- Adding values
+
+INSERT INTO category(categoryName)
+VALUES('Restaurants');
+
+INSERT INTO category(categoryName)
+VALUES('Hotels');
+
+INSERT INTO category(categoryName)
+VALUES('Television');
+
+INSERT INTO category(categoryName)
+VALUES('Movies');
+
+INSERT INTO category(categoryName)
+VALUES('Video Games');
+
+INSERT INTO category(categoryName)
+VALUES('Board Games');
