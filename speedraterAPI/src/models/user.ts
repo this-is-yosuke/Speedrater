cc31b9dbb80a6ce9cddf477d63072bc77471b5a5
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-16 20:03:02 -0700

    Created user.ts
    Created the user model

---
+++ speedraterAPI/src/models/user.ts
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(
+        public id = 0,
+        public firstName = '',
+        public lastName = '',
+        public email = '',
+        public password = ''
+    ){}
+}
