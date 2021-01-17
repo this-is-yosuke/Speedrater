ae8ac6fac885f0a0cbb4a785bf0412c42e156409
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-16 23:11:26 -0700

    Updated users.sql
    Added NOT NULL constraints

--- speedraterAPI/src/dbqueries/users.sql
+++ speedraterAPI/src/dbqueries/users.sql
@@ -2,11 +2,25 @@
 
 CREATE TABLE users (
 	userId SERIAL PRIMARY KEY,
-	firstName TEXT,
-	lastName TEXT,
+	firstName TEXT NOT NULL,
+	lastName TEXT NOT NULL,
 	email TEXT,
-	pass TEXT
+	pass TEXT NOT NULL
 );
 
 -- Adding values
-INSERT INTO users ('Joe', 'Shmoe', 'joeshmoe123@gmail.com', 'pass1')
+INSERT INTO users(firstName, lastName, email, pass) 
+VALUES('Joe', 'Shmoe', 'joeshmoe123@gmail.com', 'pass1');
+
+INSERT INTO users(firstName, lastName, email, pass) 
+VALUES('Jane', 'Doe', 'janedoe123@gmail.com', 'pass2');
+
+INSERT INTO users(firstName, lastName, email, pass) 
+VALUES('Donald', 'Trump', 'thedonald@gmail.com', 'pass3');
+
+INSERT INTO users(firstName, lastName, email, pass) 
+VALUES('Iago', 'Pereira', 'iago567@gmail.com', 'pass4');
+
+INSERT INTO users(firstName, lastName, email, pass) 
+VALUES('Kenny', 'Bui', 'bui9999@gmail.com', 'pass5');
+
