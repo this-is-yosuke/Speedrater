3fc3468e2763c8d9f2e5189593680d1d322dc0b9
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-16 21:51:14 -0700

    Updated review.ts
    Changed category and product to objects; added reviewer

--- speedraterAPI/src/models/review.ts
+++ speedraterAPI/src/models/review.ts
@@ -1,9 +1,14 @@
+import Category from './category'
+import Product from './product'
+import User from './user'
+
 export default class Review {
     constructor(
         public id = 0,
-        public category = '',
-        public product = '',
+        public category = Category,
+        public product = Product,
         public rating = 0,
-        public critique = ''
+        public critique = '',
+        public reviewer = User
     ) {}
 }
