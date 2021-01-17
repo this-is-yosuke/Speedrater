0761479c35be46b2405e0d7420e66f96bf8b8393
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-16 21:20:50 -0700

    Updated review model
    Added product and category

--- speedraterAPI/src/models/review.ts
+++ speedraterAPI/src/models/review.ts
@@ -1,6 +1,8 @@
 export default class Review {
     constructor(
         public id = 0,
+        public category = '',
+        public product = '',
         public rating = 0,
         public critique = ''
     ) {}
