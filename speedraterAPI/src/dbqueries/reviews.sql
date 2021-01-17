3a5e9264daafa532eaca6851bd33d7845cd6dd0d
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-16 21:22:38 -0700

    Created reviews.sql
    Added the sql quries used to create the Reviews table, as well as sample values

---
+++ speedraterAPI/src/dbqueries/reviews.sql
@@ -0,0 +1,12 @@
+-- Table creation
+
+CREATE TABLE reviews(
+    reviewId Serial,
+    category VARCHAR(50),
+    product VARCHAR(100),
+    rating NUMERIC,
+    critique VARCHAR(500)
+    reviewer TEXT,
+)
+
+-- Adding Values
