6f4ef3066b52b86a285ab78d631fd317a401213d
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-16 22:54:29 -0700

    Created product.sql
    Created the quesries for creating the Product table, as well as adding sample records

---
+++ speedraterAPI/src/dbqueries/product.sql
@@ -0,0 +1,18 @@
+-- Table creation
+
+CREATE TABLE product(
+    productId SERIAL PRIMARY KEY,
+    productName TEXT,
+    company TEXT
+)
+
+-- Adding values
+
+INSERT INTO product(productName, manufacturer)
+Values('Splendee', 'HiuKim Yuen');
+
+INSERT INTO product(productName, manufacturer)
+Values('McRib', "McDonald's");
+
+INSERT INTO product(productName, manufacturer)
+Values("World's Finest Hot Sauce", 'Travis Williamson');
