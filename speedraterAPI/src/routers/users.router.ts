0fbad2125f4a590eaa28cba40be46cb8ff9d4601
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-21 16:47:47 -0700

    Created users.router.ts
    Started the users router and did the findAll() method

---
+++ speedraterAPI/src/routers/users.router.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import { authMiddleware } from '../middleware/auth.middleware'
+
+export const usersRouter = express.Router();
+
+// HTTP Methods
+
+usersRouter.get('', [
+    authMiddleware('admin'),
+    async (req, res) => {
+        const users = await userDao.findAll();
+        res.json(users);
+    }
+]);
+
+usersRouter.get();
+
+usersRouter.post();
