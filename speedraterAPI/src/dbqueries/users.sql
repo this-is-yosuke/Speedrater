f7e2868f61a7cc7e6ad9f65cf437bc257bb60c4e
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-21 15:50:27 -0700

    Updated users.sql
    Added role

--- speedraterAPI/src/dbqueries/users.sql
+++ speedraterAPI/src/dbqueries/users.sql
@@ -5,22 +5,26 @@ CREATE TABLE users (
 	firstName TEXT NOT NULL,
 	lastName TEXT NOT NULL,
 	email TEXT,
-	pass TEXT NOT NULL
+	pass TEXT NOT NULL,
+	roll TEXT NOT NULL 
 );
 
 -- Adding values
-INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Joe', 'Shmoe', 'joeshmoe123@gmail.com', 'pass1');
+INSERT INTO users(firstName, lastName, email, pass, roll) 
+VALUES('Joe', 'Shmoe', 'joeshmoe123@gmail.com', 'pass1', 'user');
 
 INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Jane', 'Doe', 'janedoe123@gmail.com', 'pass2');
+VALUES('Jane', 'Doe', 'janedoe123@gmail.com', 'pass2', 'user');
 
 INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Donald', 'Trump', 'thedonald@gmail.com', 'pass3');
+VALUES('Donald', 'Trump', 'thedonald@gmail.com', 'pass3', 'user');
 
 INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Iago', 'Pereira', 'iago567@gmail.com', 'pass4');
+VALUES('Iago', 'Pereira', 'iago567@gmail.com', 'pass4', 'user');
 
 INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Kenny', 'Bui', 'bui9999@gmail.com', 'pass5');
+VALUES('Kenny', 'Bui', 'bui9999@gmail.com', 'pass5', 'user');
+
+INSERT INTO users(firstName, lastName, email, pass) 
+VALUES('Some', 'Body', 'sumbod@gmail.com', 'pass6', 'admin');
 
