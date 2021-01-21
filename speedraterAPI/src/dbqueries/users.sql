5791904433429c8725f0290f5b0d9b694561bb6b
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-21 16:46:53 -0700

    Updated users.sql
    mispelled 'role'; fixed.
    USER's reserved in SQL, changed it to 'base'.

--- speedraterAPI/src/dbqueries/users.sql
+++ speedraterAPI/src/dbqueries/users.sql
@@ -6,24 +6,24 @@ CREATE TABLE users (
 	lastName TEXT NOT NULL,
 	email TEXT,
 	pass TEXT NOT NULL,
-	roll TEXT NOT NULL 
+	roles TEXT NOT NULL 
 );
 
 -- Adding values
 INSERT INTO users(firstName, lastName, email, pass, roll) 
-VALUES('Joe', 'Shmoe', 'joeshmoe123@gmail.com', 'pass1', 'user');
+VALUES('Joe', 'Shmoe', 'joeshmoe123@gmail.com', 'pass1', 'base');
 
 INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Jane', 'Doe', 'janedoe123@gmail.com', 'pass2', 'user');
+VALUES('Jane', 'Doe', 'janedoe123@gmail.com', 'pass2', 'base');
 
 INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Donald', 'Trump', 'thedonald@gmail.com', 'pass3', 'user');
+VALUES('Donald', 'Trump', 'thedonald@gmail.com', 'pass3', 'base');
 
 INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Iago', 'Pereira', 'iago567@gmail.com', 'pass4', 'user');
+VALUES('Iago', 'Pereira', 'iago567@gmail.com', 'pass4', 'base');
 
 INSERT INTO users(firstName, lastName, email, pass) 
-VALUES('Kenny', 'Bui', 'bui9999@gmail.com', 'pass5', 'user');
+VALUES('Kenny', 'Bui', 'bui9999@gmail.com', 'pass5', 'base');
 
 INSERT INTO users(firstName, lastName, email, pass) 
 VALUES('Some', 'Body', 'sumbod@gmail.com', 'pass6', 'admin');
