fac8824ee44e48bc28204639aa81490969e62af2
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-21 18:02:09 -0700

    Updated user dao
    added method findByUsernameAndPassword()

--- speedraterAPI/src/daos/user.dao.ts
+++ speedraterAPI/src/daos/user.dao.ts
@@ -2,6 +2,28 @@ import { connectionPool } from '../util/connection'
 import { PoolClient } from 'pg'
 import User from '../models/user'
 
+// Getting name and pass for Security
+
+export async function findByUserNameAndPassword(username: String, password: String) {
+    let client: PoolClient;
+    try{
+        client = await connectionPool.connect();
+        const queryString = `SELECT * FROM users WHERE firstName = $1 AND pass = $2`;
+        const result = await client.query(queryString, [username, password]);
+        const sqlUser = result.rows[0];
+        return sqlUser && convertSqlUser(sqlUser);
+    }catch (err) {
+        console.log(err);
+    }finally {
+        client && client.release();
+    }
+    return undefined;
+};
+
+/*
+Functions For Finding
+*/ 
+
 export async function findAll() {
     let client: PoolClient;
     try{
