9209c5c7ea1812f5892e1c6860769c411b132274
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-22 17:31:33 -0700

    Updated connection.ts
    changed environment variables to caps
    added a max connection pool

--- speedraterAPI/src/util/connection.ts
+++ speedraterAPI/src/util/connection.ts
@@ -1,11 +1,21 @@
 import { Pool } from 'pg'
 
 const connectionConfig = {
-    user: process.env.Speedrater_DB_USERNAME,
-    host: process.env.Speedrater_DB_URL,
-    database: process.env.Speedrater_DB_NAME,
-    password: process.env.Speedrater_DB_PASSWORD,
-    port: +process.env.Speedrater_DB_PORT
+    user: process.env.SPEEDRATER_DB_USERNAME,
+    host: process.env.SPEEDRATER_DB_URL,
+    database: process.env.SPEEDRATER_DB_NAME,
+    password: process.env.SPEEDRATER_DB_PASSWORD,
+    port: +process.env.SPEEDRATER_DB_PORT,
+    max: 5
 };
 
 export const connectionPool = new Pool(connectionConfig);
+
+// 8050 is not a free port; apparently it's for Transmission Control Protocol
+// 8000 is for internet radio streaming
+
+/* Furthermore, if you run the app on an occupied port, you'll run 
+into the error: Connection Terminated Unexpectedly */
+
+/* I am an idiot. The port here is for PG's port, which is set to 
+5432. The port in index.ts is for the API. */
