302325f4f5a360315d57a60ec16ec775eef01f7b
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-22 17:33:31 -0700

    Updated review model
    changed = to : for classes
    added {} to non-default imports

--- speedraterAPI/src/models/review.ts
+++ speedraterAPI/src/models/review.ts
@@ -1,15 +1,15 @@
-import Category from './category'
-import Product from './product'
+import { Category } from './category'
+import { Product } from './product'
 import User from './user'
 
 export default class Review {
     constructor(
         public id = 0,
-        public category = Category,
-        public product = Product,
+        public category: Category,
+        public product: Product,
         public rating = 0,
         public critique = '',
-        public reviewer = User,
+        public reviewer: User,
         public reviewDate = ''
     ) {}
 }
