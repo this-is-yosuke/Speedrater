cdc4fd411fe3237ec16b07f680699843782f95cd
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-26 13:36:28 -0700

    Updateed user router
    added a post function

--- speedraterAPI/src/routers/users.router.ts
+++ speedraterAPI/src/routers/users.router.ts
@@ -1,19 +1,33 @@
 import express from 'express'
 import * as userDao from '../daos/user.dao'
-import { authMiddleware } from '../middleware/auth.middleware'
+// import { authMiddleware } from '../middleware/auth.middleware'
 
 export const usersRouter = express.Router();
 
 // HTTP Methods
 
 usersRouter.get('', [
-    authMiddleware('admin'),
+    // authMiddleware('admin'),
     async (req, res) => {
         const users = await userDao.findAll();
         res.json(users);
     }
 ]);
 
-usersRouter.get();
+usersRouter.post('', async(req, res) => {
+    const user = req.body;
+    if(!user) {
+        res.sendStatus(400);
+    }else{
+        const id = await userDao.save(user);
+        if(!id) {
+            res.sendStatus(400);
+        }else{
+            user.id = id;
+            res.status(201);
+            res.json(user);
+        }
+    }
+});
 
-usersRouter.post();
+// usersRouter.post();
