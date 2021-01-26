76cfc1f514aa84a5568f24f71bf82359c6af6df9
this-is-yosuke <yosuke.kibe@gmail.com>
2021-01-25 17:47:22 -0700

    Updated review converter
    got rid of 'row.categoryid &&'
    and 'row.userid &&'. Wasn't working in Postman.

--- speedraterAPI/src/util/review.converter.ts
+++ speedraterAPI/src/util/review.converter.ts
@@ -6,8 +6,8 @@ import User from '../models/user'
 export function reviewConverter(row) {
     return new Review(row.reviewid,
         new Category(row.categoryid, row.categoryname),
-        row.categoryid && new Product(row.productid, row.productname, row.company),
+        new Product(row.productid, row.productname, row.company),
         row.rating, row.critique,
-        row.userid && new User(row.userid, row.firstname, row.lastname, row.email, row.pass, row.roles),
+        new User(row.userid, row.firstname, row.lastname, row.email, row.pass, row.roles),
         row.reviewdate)
 }
